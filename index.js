19bc3604b1e578c3d5297f4d1a5cbe8dd996ebf4
moataz osama <moataz.osama@hotmail.com>
2020-04-02 17:48:33 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+function takeANumber(currentline,name){
+  var i=0
+  // var katzDeliLine = []
+  while(i<currentline.length){
+    return (`Welcome, ${currentline[i]}. You are number ${i +1} in line`)
+
+i++
+  }
+}
+// function nowServing(katzDeliLine){
+//   var i=o
+// while (katzDeliLine.length>0){
+//   return
+//
+// }

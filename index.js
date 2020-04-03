1f8faad094f6c393283a32a35bb82cb40f0a120d
moataz osama <moataz.osama@hotmail.com>
2020-04-03 15:08:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,12 +10,12 @@
 // }
 function nowServing(katzDeliLine){
   var i=0
-  if(katzDeliLine.length<0){
+  if(katzDeliLine.length===0){
   return "There is nobody waiting to be served!"}
   else {
     for(i=0;i<katzDeliLine.length;i++){
-
       katzDeliLine.shift()
   return katzDeliLine[0]
   }
-}}
+}
+}
